8c2904235f0b7ca23acdbb9ab1e1c4c3132313ab
Mitchell Garate <69022453+Dean148@users.noreply.github.com>
2025-02-01 16:14:55 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -7,6 +7,6 @@ export let PROXY_CODE_SECRET = 'Easy-Reverse-Proxy'; // 代理码密钥
 export let PROXIES: Proxy[] = [
   {
     domain: "google.deannet.top",
-    url: "https://www.google.com"
+    url: "https://www.google.com/"
   }
 ];
